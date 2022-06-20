635ff6a03341da3c4c744bf8591761507517297a
nakachukwu.github.io <nonso.o@icloud.com>
2022-06-20 17:02:30 +0100

    Addition of scripts.

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
-let name = "Youtube Clone";
-console.log();
+let welcomeMessage = "Youtube Clone";
+alert(welcomeMessage);
+
+//let name = "Nonso";
+
+// Naming conventions
+//let message = "This is my first message" // ✅
+//let message2 = "This is my second message" // ✅
+//let user_name = "AdeJ123" // ✅
+//let myReallyLongName = "Ade Johnson" // ✅
+
+// let 1message = "This is my first message" // ❌
+// let user-name = "AdeJ123" // ❌
+// let myreallylongname = "Ade Johnson" // ❌
+
+// Variable declaration (const)
+// const NAME = "Ade Johnson"
+//NAME = "Adewale Johnson" //❌ //error, cannot re-assign to a constant variable
+
+//const NAME = "Ade Johnson" // ✅
+//const name = "Ade Johnson" // ❌
+
+//const MY_BIRTHDAY = "5th July 2000" // ✅
+//const MYBIRTHDAY = "5th July 2000" // ❌
+
+// Variable declaration (let)
+//let name = "Ade Johnson"
+//name = "Adewale Johnson" //✅
+
+//let fullName = "Ade Johnson";
+//alert("oldname " + fullName);
+
+//fullName = "Adebisi Ademola";
+//alert("newname " + fullName);
+
+//const myBirthday = "5th July 2000" // ✅
+//const mybirthday = "5th July 2000" // ❌
